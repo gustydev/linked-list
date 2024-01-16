0cfc214e79c8552b7dbc791f226d2c513d54e84e
gusty <gustavodiorlando@gmail.com>
2024-01-16 14:37:37 -0300

    Add prepend function

--- linkedList.js
+++ linkedList.js
@@ -6,11 +6,25 @@ class Node {
 }
 
 class LinkedList {
-    constructor(head) {
-        this.head = new Node(head);
+    constructor() {
+        this.head = null;
+    }
+    prepend(value) {
+        if (this.head === null) {
+            this.head = new Node(value);
+        } else {
+            this[this.head.value] = new Node(this.head.value, this.head.next);
+            this.head = new Node(value, this.head.value);
+        }
     }
 }
 
-console.log(new Node('test'))
-console.log(new LinkedList())
+const test = new LinkedList();
+console.log(test)
+test.prepend('test 1')
+console.log(test)
+test.prepend('test 2')
+console.log(test)
+test.prepend('test 3')
+console.log(test)
 // {head: Node('hahaha', 1), 1: Node('hihihi', 2), 2: Node('hohoho', 3), tail: Node('huhuhu', null)}
